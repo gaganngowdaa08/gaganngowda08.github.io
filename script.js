433cb9aa4f7c949146806d20b34cba10a8e22951
Gagan Gowda <gaganngowdaa08@gmail.com>
2026-02-18 15:54:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,5 @@
-// Mouse glow
-const glow = document.querySelector('.cursor-glow');
-document.addEventListener('mousemove', e=>{
-    glow.style.left = e.clientX + 'px';
-    glow.style.top = e.clientY + 'px';
+function scrollToContact(){
+document.getElementById("contact").scrollIntoView({
+behavior:"smooth"
 });
-
-// Typing animation
-const text = "AI & Cloud Engineer";
-let i = 0;
-function typing(){
-    if(i < text.length){
-        document.querySelector(".typing").innerHTML += text.charAt(i);
-        i++;
-        setTimeout(typing,80);
-    }
-}
-typing();
-
-// Modal functions
-function openModal(id){
-    document.getElementById(id).style.display="flex";
-}
-function closeModal(id){
-    document.getElementById(id).style.display="none";
 }
